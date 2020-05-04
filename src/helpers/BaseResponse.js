746269afa2be62c2c4afc95ca8d283fa320143f4
Everton Rodrigues Resende <ecoresende@hotmail.com>
2020-05-04 17:48:15 -0300

    node error

--- src/helpers/BaseResponse.js
+++
@@ -1,7 +0,0 @@
-export class BaseResponse {
-    constructor(code,message,data) {
-        this.code = code
-        this.message = message
-        this.data = data
-    }
-}
--- src/helpers/FatalErrorResponse.js
+++
@@ -1,7 +0,0 @@
-export class FatalErrorResponse {
-    constructor(data) {
-        this.code = "ERR-0001"
-        this.message = "Erro interno - Favor informar o administrador do sistema"
-        this.data = data
-    }
-  }
